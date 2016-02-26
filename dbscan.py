379418caaf0144b2e9b35d037ce453cf689e9959
wrudebusch <wrudebusch@gmail.com>
2016-02-26 11:10:10 -0600

    DBSCAN of NYPD traffic data
    scikit-learn exmaple code

---
+++ dbscan.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+===================================
+Demo of DBSCAN clustering algorithm
+===================================
+
+Finds core samples of high density and expands clusters from them.
+
+"""
+print(__doc__)
+
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+
+##############################################################################
+# Read in data
+
+raw_data = pd.read_json('https://data.cityofnewyork.us/resource/h9gi-nx95.json')
+data = raw_data[['latitude', 'longitude', 'number_of_pedestrians_injured']].dropna()
+X = data.as_matrix()
+
+##############################################################################
+# Compute DBSCAN, larger min_samples = fewer clusters, look up eps
+
+db = DBSCAN(eps=0.01, min_samples=10).fit(X)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+print('Estimated number of clusters: %d' % n_clusters_)
+
+
+##############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = 'k'
+
+    class_member_mask = (labels == k)
+
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
